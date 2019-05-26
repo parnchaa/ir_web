4d89a0014d77314a474dc7e197bdc1d25a407ffa
Unknown <parnchaa@gmail.com>
2019-05-26 23:03:36 +0700

    add build

---
+++ test/build/precache-manifest.3e7ca02d776824a89fa8191cbee779ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "77d42129bd1e1114ec4ee165eef3e5a7",
+    "url": "/static/media/ir-parking.77d42129.jpg"
+  },
+  {
+    "revision": "bee211ee580467c857a798f186049730",
+    "url": "/static/media/ceo.bee211ee.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "f36a2ca003e0baa31e9f",
+    "url": "/static/js/main.f36a2ca0.chunk.js"
+  },
+  {
+    "revision": "aea0d09bc75ff9478b72",
+    "url": "/static/js/2.aea0d09b.chunk.js"
+  },
+  {
+    "revision": "f36a2ca003e0baa31e9f",
+    "url": "/static/css/main.d440e255.chunk.css"
+  },
+  {
+    "revision": "aea0d09bc75ff9478b72",
+    "url": "/static/css/2.ef3daa19.chunk.css"
+  },
+  {
+    "revision": "077f829db513410ca346d139bbfc6255",
+    "url": "/index.html"
+  }
+];
