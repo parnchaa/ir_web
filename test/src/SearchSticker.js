801c0d43875ac04c4533d91fa0740992d7b9702a
nutchaphon <nutchaphon.eiei@gmail.com>
2019-07-21 16:33:13 +0700

    update

--- test/src/SearchSticker.js
+++ test/src/SearchSticker.js
@@ -1,91 +1,180 @@
-import React,{Component} from 'react';
-import Header from './Header';
-import Navibar from './Navibar';
-import './SearchSticker.css'
-import Modal from 'react-responsive-modal';
+import React, { Component } from "react";
+import Header from "./Header";
+import Navibar from "./Navibar";
+import "./SearchSticker.css";
+import Modal from "react-responsive-modal";
 
-
-class SearchSticker extends Component{
-    constructor(props) {
-        super(props)
-        this.state = {
-            carOwner:[],
-            openEdit: false
-        }
-    }
-    componentDidMount() {
-        fetch('http://localhost:5000/carOwner')
-            .then((response) => {
-                return response.json();
-            })
-            .then((carOwner) => {
-                this.setState({ carOwner })
-                console.log("carOwner", this.state.carOwner)
-            });
-    }
-
-    onOpenEditModal = (ruleID) => (e) => {
-      this.setState({
-          openEdit: true,
-          openRuleID: ruleID
-      });
+class SearchSticker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carOwner: [],
+      openEdit: false,
+      carOwnerFirstName:'',
+      carOwnerLastName:'',
+      carOwnerEmail:'',
+      carOwnerTel:''
     };
+  }
+  componentDidMount() {
+    fetch("http://localhost:5000/carOwner")
+      .then(response => {
+        return response.json();
+      })
+      .then(carOwner => {
+        this.setState({ carOwner });
+        console.log("carOwner", this.state.carOwner);
+      });
+  }
 
-    carOwnerTable() {
-        return this.state.carOwner.map((carOwner) => {
-            const { carOwnerFirstName, carOwnerLastName, carOwnerTel, carOwnerEmail, carOwnerAddress } = carOwner
-            return (
-                <div>
-                <Modal  open={this.state.openAddSecurityguard} onClose={this.onCloseAddSecurityguardModal} center>
-                    <h2>AddSecurityguard </h2>
-                    <form>l
-                        <p>FirstName: </p>
-                        <input type="text" name='firstName' onChange={event => this.handleChange(event)} value={this.state.firstName} />
-                        <p>LastName: </p>
-                        <input type="text" name='lastName' onChange={event => this.handleChange(event)} value={this.state.lastName} />
-                        <p>Email: </p>
-                        <input type="text" name='staffTel' onChange={event => this.handleChange(event)} value={this.state.staffTel} />
-                        <p>Phone number: </p>
-                        <input type="text" name='staffEmail' onChange={event => this.handleChange(event)} value={this.state.staffEmail} />
-                        <div></div>
-                    </form>
-                    <button onClick={event => this.handleSubmitSecurity(event)}>Add</button>
-                    <button onClick={this.onCloseAddSecurityguardModal}>Cancel</button>
-                </Modal>
-                <tr>
-                    <td>{carOwnerFirstName}</td>
-                    <td>{carOwnerLastName}</td>
-                    <td>{carOwnerTel}</td>
-                    <td>{carOwnerEmail}</td>
-                    <td>{carOwnerAddress}</td>
-                    <td><button className='EditModalButton' onClick={this.onOpenEditModal(carOwner)}>Edit</button></td>
-                </tr>
-                </div>
-            )
-        }
-        )
-    }
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({
+        [event.target.name]: event.target.value
 
+    })
+    console.log("rr", event.target.name);
+    console.log("rr", event.target.value);
+}
 
-    render() {
+  handleEditCarOwner = event => {
+    event.preventDefault();
+    this.onAfterEditCarOwner();
+    this.setState({
+      carOwnerFirstName: "",
+      carOwnerLastName: "",
+      carOwnerEmail: "",
+      carOwnerTel: "",
+      openEdit: false
+    });
+  };
 
-     return(
-         <div>
-            <Header/>
-            <Navibar/>
-            <table className='table'>
-                    <tbody>
-                        {this.carOwnerTable()}
-                    </tbody>
-            </table>
+  onAfterEditCarOwner = () => {
+    const url = 'http://localhost:5000/editCarOwner';
+    const bodyData = JSON.stringify({
+      carOwnerFirstName: this.state.carOwnerFirstName,
+      carOwnerLastName: this.state.carOwnerLastName,
+      carOwnerEmail: this.state.carOwnerEmail,
+      carOwnerTel: this.state.carOwnerTel
+    });
+    console.log(bodyData)
+    const othepram = {
+        headers: {
+            "content-type": "application/json; charset=UTF-8"
+        },
+        body: bodyData,
+        method: "POST"
+    };
+    fetch(url, othepram)
+        .then(data => console.log(data))
+}
 
+  onOpenEditModal = ruleID => e => {
+    this.setState({
+      openEdit: true,
+      openRuleID: ruleID
+    });
+  };
 
-         </div>
+  onCloseEditModal = () => {
+    this.setState({ openEdit: false });
+  };
 
-     );
+  carOwnerTable() {
+    return this.state.carOwner.map(carOwner => {
+      const {
+        carOwnerFirstName,
+        carOwnerLastName,
+        carOwnerTel,
+        carOwnerEmail,
+        carOwnerAddress,
+        registerDate,
+        expiredDate
+      } = carOwner;
+      return (
+        <div>
+          <Modal
+            open={this.state.openEdit}
+            onClose={this.onCloseEditModal}
+            center
+          >
+            <h2>Edit CarOwner </h2>
+            <form>
+              <div className='editModal'>
+                <p>FirstName: </p>
+                <input
+                  type="text"
+                  name="carOwnerFirstName"
+                  onChange={event => this.handleChange(event)}
+                  value={carOwnerFirstName}
+                />
+              </div>
+              <div className='editModal'>
+                <p>LastName: </p>
+                <input
+                  type="text"
+                  name="carOwnerLastName"
+                  onChange={event => this.handleChange(event)}
+                  value={carOwnerLastName}
+                />
+              </div>
+              <div className='editModal'>
+                <p>Email: </p>
+                <input
+                  type="text"
+                  name="carOwnerEmail"
+                  onChange={event => this.handleChange(event)}
+                  value={carOwnerEmail}
+                />
+              </div>
+              <div className='editModal'>
+                <p>Phone number: </p>
+                <input
+                  type="text"
+                  name="carOwnerTel"
+                  onChange={event => this.handleChange(event)}
+                  value={carOwnerTel}
+                />
+              </div>
+            </form>
+            <button onClick={event => this.handleEditCarOwner(event)}>
+              Update
+            </button>
+            <button onClick={this.onCloseEditModal}>Cancel</button>
+          </Modal>
+          <tr>
+            <td>{carOwnerFirstName}</td>
+            <td>{carOwnerLastName}</td>
+            <td>{carOwnerEmail}</td>
+            <td>{carOwnerTel}</td>
+            <td>{carOwnerAddress}</td>
+            <td>{registerDate}</td>
+            <td>{expiredDate}</td>
+            <td>
+              <button
+                className="EditModalButton"
+                onClick={this.onOpenEditModal(carOwner)}
+              >
+                Edit
+              </button>
+            </td>
+          </tr>
+        </div>
+      );
+    });
+  }
 
-    } 
-
+  render() {
+    return (
+      <div>
+        <Header />
+        <Navibar />
+        <table className="table">
+          <tbody>{this.carOwnerTable()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default SearchSticker;
--- test/src/Staff.js
+++ test/src/Staff.js
@@ -1,260 +1,277 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import Navibar from './Navibar';
-import './Staff.css'
-import Modal from 'react-responsive-modal';
+import React, { Component } from "react";
+import Header from "./Header";
+import Navibar from "./Navibar";
+import "./Staff.css";
+import Modal from "react-responsive-modal";
 
 class Staff extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            staff: [],
-            securityguard: [],
-            firstName: '',
-            lastName: '',
-            staffEmail: '',
-            staffTel: '',
-            openAdd: false,
-            openAddSecurityguard: false
-        }
-    }
-
-    componentDidMount() {
-        fetch('http://localhost:5000/staff')
-            .then((response) => {
-                return response.json();
-            })
-            .then((staff) => {
-                this.setState({ staff })
-                console.log("staff1", this.state.staff)
-            });
-        fetch('http://localhost:5000/securityguard')
-            .then((response) => {
-                return response.json();
-            })
-            .then((securityguard) => {
-                this.setState({ securityguard })
-                console.log("securityguard", this.state.securityguard)
-            });
-    }
-
-    staffTable() {
-        return this.state.staff.map((staff) => {
-            const { firstName, lastName, staffEmail, staffTel } = staff
-            return (
-                <tr>
-                    <td>{firstName}</td>
-                    <td>{lastName}</td>
-                    <td>{staffEmail}</td>
-                    <td>{staffTel}</td>
-                </tr>
-            )
-        }
-        )
-    }
-
-    onOpenAddModal = () => {
-        this.setState({ openAdd: true });
+  constructor(props) {
+    super(props);
+    this.state = {
+      staff: [],
+      securityguard: [],
+      firstName: "",
+      lastName: "",
+      staffEmail: "",
+      staffTel: "",
+      openAdd: false,
+      openAddSecurityguard: false
     };
+  }
 
-    onCloseAddModal = () => {
-        this.setState({ openAdd: false });
+  componentDidMount() {
+    fetch("http://localhost:5000/staff")
+      .then(response => {
+        return response.json();
+      })
+      .then(staff => {
+        this.setState({ staff });
+        console.log("staff1", this.state.staff);
+      });
+    fetch("http://localhost:5000/securityguard")
+      .then(response => {
+        return response.json();
+      })
+      .then(securityguard => {
+        this.setState({ securityguard });
+        console.log("securityguard", this.state.securityguard);
+      });
+  }
+
+  staffTable() {
+    return this.state.staff.map(staff => {
+      const { firstName, lastName, staffEmail, staffTel } = staff;
+      return (
+        <tr>
+          <td>{firstName}</td>
+          <td>{lastName}</td>
+          <td>{staffEmail}</td>
+          <td>{staffTel}</td>
+        </tr>
+      );
+    });
+  }
+
+  onOpenAddModal = () => {
+    this.setState({ openAdd: true });
+  };
+
+  onCloseAddModal = () => {
+    this.setState({ openAdd: false });
+  };
+
+  onOpenAddSecurityguardModal = () => {
+    this.setState({ openAddSecurityguard: true });
+  };
+
+  onCloseAddSecurityguardModal = () => {
+    this.setState({ openAddSecurityguard: false });
+  };
+
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    console.log("rr", event.target.name);
+    console.log("rr", event.target.value);
+  };
+
+  handleSubmitAdmin = event => {
+    event.preventDefault();
+    this.onAfterInsertAdmin();
+    this.setState({
+      firstName: "",
+      lastName: "",
+      staffEmail: "",
+      staffTel: "",
+      openAdd: false
+    });
+  };
+
+  onAfterInsertAdmin = () => {
+    const url = "http://localhost:5000/addstaff";
+    const bodyData = JSON.stringify({
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      staffEmail: this.state.staffEmail,
+      staffTel: this.state.staffTel
+    });
+    console.log(bodyData);
+    const othepram = {
+      headers: {
+        "content-type": "application/json; charset=UTF-8"
+      },
+      body: bodyData,
+      method: "POST"
     };
+    fetch(url, othepram).then(data => console.log(data));
+  };
 
-    onOpenAddSecurityguardModal = () => {
-        this.setState({ openAddSecurityguard: true });
+  handleSubmitSecurity = event => {
+    event.preventDefault();
+    this.onAfterInsertSecurity();
+    this.setState({
+      firstName: "",
+      lastName: "",
+      staffEmail: "",
+      staffTel: "",
+      openAddSecurityguard: false
+    });
+  };
+  onAfterInsertSecurity = () => {
+    const url = "http://localhost:5000/addsecurityguard";
+    const bodyData = JSON.stringify({
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      staffEmail: this.state.staffEmail,
+      staffTel: this.state.staffTel
+    });
+    console.log(bodyData);
+    const othepram = {
+      headers: {
+        "content-type": "application/json; charset=UTF-8"
+      },
+      body: bodyData,
+      method: "POST"
     };
+    fetch(url, othepram).then(data => console.log(data));
+  };
 
-    onCloseAddSecurityguardModal = () => {
-        this.setState({ openAddSecurityguard: false });
-    };
+  securityguardTable() {
+    return this.state.securityguard.map(securityguard => {
+      const { firstName, lastName, staffEmail, staffTel } = securityguard;
+      return (
+        <tr>
+          <td>{firstName}</td>
+          <td>{lastName}</td>
+          <td>{staffEmail}</td>
+          <td>{staffTel}</td>
+        </tr>
+      );
+    });
+  }
 
-    handleChange = (event) => {
-        event.preventDefault();
-        this.setState({
-            [event.target.name]: event.target.value
-
-        })
-        console.log("rr", event.target.name);
-        console.log("rr", event.target.value);
-    }
-
-    handleSubmitAdmin = (event) => {
-        event.preventDefault();
-        this.onAfterInsertAdmin();
-        this.setState({
-            firstName: '',
-            lastName: '',
-            staffEmail: '',
-            staffTel: '',
-            openAdd: false
-        })
-    }
-
-    onAfterInsertAdmin = () => {
-        const url = 'http://localhost:5000/addstaff';
-        const bodyData = JSON.stringify({
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            staffEmail: this.state.staffEmail,
-            staffTel: this.state.staffTel
-        });
-        console.log(bodyData)
-        const othepram = {
-            headers: {
-                "content-type": "application/json; charset=UTF-8"
-            },
-            body: bodyData,
-            method: "POST"
-        };
-        fetch(url, othepram)
-            .then(data => console.log(data))
-    }
-
-
-
-    handleSubmitSecurity = (event) => {
-        event.preventDefault();
-        this.onAfterInsertSecurity();
-        this.setState({
-            firstName: '',
-            lastName: '',
-            staffEmail: '',
-            staffTel: '',
-            openAddSecurityguard: false
-        })
-    }
-    onAfterInsertSecurity = () => {
-        const url = 'http://localhost:5000/addsecurityguard';
-        const bodyData = JSON.stringify({
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            staffEmail: this.state.staffEmail,
-            staffTel: this.state.staffTel
-        });
-        console.log(bodyData)
-        const othepram = {
-            headers: {
-                "content-type": "application/json; charset=UTF-8"
-            },
-            body: bodyData,
-            method: "POST"
-        };
-        fetch(url, othepram)
-            .then(data => console.log(data))
-    }
-
-    securityguardTable() {
-        return this.state.securityguard.map((securityguard) => {
-            const { firstName, lastName, staffEmail, staffTel } = securityguard
-            return (
-                <tr>
-                    <td>{firstName}</td>
-                    <td>{lastName}</td>
-                    <td>{staffEmail}</td>
-                    <td>{staffTel}</td>
-                </tr>
-            )
-        }
-        )
-    }
-
-    render() {
-
-        return (
-            <div>
-                <Header />
-                <Navibar />
-                <h2>Admin</h2>
-                <button className='addStaff' onClick={this.onOpenAddModal}>Add Admin</button>
-                <Modal open={this.state.openAdd} onClose={this.onCloseAddModal} center>
-                    <h2>Add Staff</h2>
-                    <form onSubmit={this.handleSubmitAdmin}>
-                        <table className='tableModal'>
-                            <tr>
-                                <td>
-                                    <label htmlFor="firstName">FirstName: </label >
-                                </td>
-                                <td>
-                                    <input type="text" name='firstName' onChange={event => this.handleChange(event)} value={this.state.firstName} />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <label htmlFor="lastName">LastName: </label>
-                                </td>
-                                <td>
-                                    <input type="text" name='lastName' onChange={event => this.handleChange(event)} value={this.state.lastName} />
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    <label htmlFor="staffTel">Phone number: </label>
-                                </td>
-                                <td>
-                                    <input type="text" name='staffTel' onChange={event => this.handleChange(event)} value={this.state.staffTel} />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <label htmlFor="staffEmail">Email: </label>
-                                </td>
-                                <td>
-                                    <input type="text" name='staffEmail' onChange={event => this.handleChange(event)} value={this.state.staffEmail} />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <button className='modalAdd' onClick={event => this.handleSubmitAdmin(event)} type="submit">Add</button>
-                                    <button className='modalcancle' onClick={this.onCloseAddModal}>Cancel</button>
-                                </td>
-                            </tr>
-                        </table>
-                    </form>
-
-
-
-                </Modal>
-
-                <table className='staffTable'>
-                    <tbody>
-                        {this.staffTable()}
-                    </tbody>
-                </table>
-
-                <h2>Security Guard</h2>
-                <button className='addSecurityguard' onClick={this.onOpenAddSecurityguardModal}>Add Securityguard </button>
-                <Modal  open={this.state.openAddSecurityguard} onClose={this.onCloseAddSecurityguardModal} center>
-                    <h2>AddSecurityguard </h2>
-                    <form>
-                        <p>FirstName: </p>
-                        <input type="text" name='firstName' onChange={event => this.handleChange(event)} value={this.state.firstName} />
-                        <p>LastName: </p>
-                        <input type="text" name='lastName' onChange={event => this.handleChange(event)} value={this.state.lastName} />
-                        <p>Email: </p>
-                        <input type="text" name='staffTel' onChange={event => this.handleChange(event)} value={this.state.staffTel} />
-                        <p>Phone number: </p>
-                        <input type="text" name='staffEmail' onChange={event => this.handleChange(event)} value={this.state.staffEmail} />
-                        <div></div>
-                    </form>
-                    <button onClick={event => this.handleSubmitSecurity(event)}>Add</button>
-                    <button onClick={this.onCloseAddSecurityguardModal}>Cancel</button>
-                </Modal>
-                <table className='staffTable'>
-                    <tbody>
-                        {this.securityguardTable()}
-                    </tbody>
-                </table>
-
-
+  render() {
+    return (
+      <div>
+        <Header />
+        <Navibar />
+        <h2>Admin</h2>
+        <button className="addStaff" onClick={this.onOpenAddModal}>
+          Add Admin
+        </button>
+        <Modal open={this.state.openAdd} onClose={this.onCloseAddModal} center>
+          <h2>Add Staff</h2>
+          <form className='formAdd' onSubmit={this.handleSubmitAdmin}>
+            <div className='addModal'>
+            <label htmlFor="firstName">FirstName: </label>
+            <input
+              type="text"
+              name="firstName"
+              onChange={event => this.handleChange(event)}
+              value={this.state.firstName}
+            />
             </div>
+            <div className='addModal'>
+            <label htmlFor="lastName">LastName: </label>
+            <input
+              type="text"
+              name="lastName"
+              onChange={event => this.handleChange(event)}
+              value={this.state.lastName}
+            />
+            </div>
+            <div className='addModal'>
+            <label htmlFor="staffTel">Phone number: </label>
+            <input
+              type="text"
+              name="staffTel"
+              onChange={event => this.handleChange(event)}
+              value={this.state.staffTel}
+            />
+            </div>
+            <div className='addModal'>
+            <label htmlFor="staffEmail">Email: </label>
+            <input
+              type="text"
+              name="staffEmail"
+              onChange={event => this.handleChange(event)}
+              value={this.state.staffEmail}
+            />
+            </div>
+            <button
+              className="modalAdd"
+              onClick={event => this.handleSubmitAdmin(event)}
+              type="submit"
+            >
+              Add
+            </button>
+            <button className="modalcancel" onClick={this.onCloseAddModal}>
+              Cancel
+            </button>
+          </form>
+        </Modal>
 
-        );
+        <table className="staffTable">
+          <tbody>{this.staffTable()}</tbody>
+        </table>
 
-    }
-
+        <h2>Security Guard</h2>
+        <button
+          className="addSecurityguard"
+          onClick={this.onOpenAddSecurityguardModal}
+        >
+          Add Securityguard{" "}
+        </button>
+        <Modal
+          open={this.state.openAddSecurityguard}
+          onClose={this.onCloseAddSecurityguardModal}
+          center
+        >
+          <h2>AddSecurityguard </h2>
+          <form>
+            <p>FirstName: </p>
+            <input
+              type="text"
+              name="firstName"
+              onChange={event => this.handleChange(event)}
+              value={this.state.firstName}
+            />
+            <p>LastName: </p>
+            <input
+              type="text"
+              name="lastName"
+              onChange={event => this.handleChange(event)}
+              value={this.state.lastName}
+            />
+            <p>Email: </p>
+            <input
+              type="text"
+              name="staffTel"
+              onChange={event => this.handleChange(event)}
+              value={this.state.staffTel}
+            />
+            <p>Phone number: </p>
+            <input
+              type="text"
+              name="staffEmail"
+              onChange={event => this.handleChange(event)}
+              value={this.state.staffEmail}
+            />
+            <div />
+          </form>
+          <button onClick={event => this.handleSubmitSecurity(event)}>
+            Add
+          </button>
+          <button onClick={this.onCloseAddSecurityguardModal}>Cancel</button>
+        </Modal>
+        <table className="staffTable">
+          <tbody>{this.securityguardTable()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Staff;
